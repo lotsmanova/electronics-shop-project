53f51618be5d4f2aec3f0d0fea7ea5cd9ca483bd
Violetta <lotsmanovavioletta@gmail.com>
2023-05-14 15:29:14 +0300

    add instantiate_from_csv(), string_to_number(), getter and setter for name

--- src/item.py
+++ src/item.py
@@ -1,3 +1,5 @@
+import csv
+import os
 class Item:
     """
     Класс для представления товара в магазине.
@@ -14,10 +16,30 @@ class Item:
         :param price: Цена за единицу товара.
         :param quantity: Количество товара в магазине.
         """
-        self.name = name
+        self.__name = name
         self.price = price
         self.quantity = quantity
         self.__class__.all.append(self)
+
+
+    @property
+    def name(self):
+        """
+        Геттер для атрибута name
+        """
+        return self.__name
+
+
+    @name.setter
+    def name(self, name):
+        """
+        Сеттер для атрибута name
+        """
+        if len(name) <= 10:
+            self.__name = name
+        else:
+            raise Exception("Длина наименования товара превышает 10 символов")
+
 
 
     def calculate_total_price(self) -> float:
@@ -34,3 +56,19 @@ class Item:
         Применяет установленную скидку для конкретного товара.
         """
         self.price *= self.pay_rate
+
+
+    @classmethod
+    def instantiate_from_csv(cls):
+        PATH_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+        PATH_CSV = os.path.join(PATH_DIR, 'src', 'items.csv')
+
+        with open(PATH_CSV, newline = '') as csvfile:
+            reader = csv.DictReader(csvfile)
+            for row in reader:
+                cls(row['name'], row['price'], row['quantity'])
+
+
+    @staticmethod
+    def string_to_number(num: str):
+        return int(float(num))
--- tests/test_item.py
+++ tests/test_item.py
@@ -19,3 +19,18 @@ def test_apply_discount_rate(item1):
     Item.pay_rate = 0.8
     item1.apply_discount()
     assert item1.price == 8000.0
+
+
+def tes_name_1(item1):
+    assert item1.name == 'Смартфон'
+
+
+def test_name_2(item1):
+    item1.name = 'Ноутбук'
+    assert item1.name == 'Ноутбук'
+
+
+def test_string_to_number():
+    assert Item.string_to_number('5') == 5
+    assert Item.string_to_number('5.5') == 5
+    assert Item.string_to_number('5.0') == 5
